8abdfaed25284c4cfac05b5f88933aa5c5a920f5
Allen Zhou <allenxzhou@berkeley.edu>
2015-08-07 13:42:21 -0700

    Updated User Prompt for bikeScrapy

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Bike Scrapy
 Status:
-- [ ] Updating based on Craigslist HTML calls
+- [ ] Adding in a terminal user prompt
+- [ ] Updating code functionality (e.g. Craigslist HTML changes)
 - [ ] Adding in a search functionality (instead of hardcoded)
 
 Backstory: In the Summer of 2013, I embarked on what I thought would be a long and arduous journey to find myself a new bicycle. I was in the market for a road bike with specific features (i.e. 52'' frame, good brand, 105 or better shifters). However, after a few days of manually clicking on over a hundred Craigslist ads, my cramped hand relegated the bike searching task to my programming brain. My brain then put my cramped hand (and other perfectly relaxed hand) back to work to produce code that would scrape Craigslist ads based on the feature set I desired. And thus, the bike scraper (recently named "Bike Scrapy") was born.
 
-*This bike scraper is built from scrap. I thought about using Scrapy, but I figured creating a scraper from scratch would be more enjoyable than necessarily calling an API. Maybe I'll switch over to Scrapy in the future... Maybe.*
+*This bike scraper is built from scratch. I thought about using Scrapy, but I figured creating a scraper from ground-up would be more enjoyable than necessarily calling an API. Maybe I'll switch over to Scrapy in the future... Maybe.*
 
--- bikeScrapy.py
+++ bikeScrapy.py
@@ -1,5 +1,7 @@
 # UI Prompt for Bike Scrapy
 
+#import bikes
+#import openBikes
 import re
 import requests
 import sys
@@ -10,16 +12,39 @@ class BikeScrapy:
         self.brand = brand
         self.size = size
         self.shifters = shifters
+        self.options = {'y': '',
+                        'o': '',
+                        's': '',
+                        'n': lambda: None}
+
+    def search(self):
+        return False
+
+    def url_action(self, option):
+        return False
 
 
 if __name__ == "__main__":
     print "Press CTRL+C at any time to exit."
+    promptFeature = lambda feature: raw_input(feature + '\n>>> ')
+    
     while True:
         try:
             print "*Currently customized primarily for road bikes"
-            brand = raw_input('BRAND:\n')
-            size = raw_input('FRAME SIZE:\n')
-            shifters = raw_input('SHIFTERS:\n')
+            brand = promptFeature('BRAND')
+            size = promptFeature('SIZE')
+            shifters = promptFeature('SHIFTERS')
             print "Generating URLs..."
+            bike_scrapy = BikeScrapy(brand, size, shifters)
+
+            while True:
+                option = promptFeature('URLs: Open/Save? ' +
+                    r"(Options = 'y', 'o', 's', 'n')")
+
+                if option in bike_scrapy.options:
+                    bike_scrapy.options[option]
+                    break
+                else:
+                    print(r"Please enter 'y', 'o', 's', or 'n'")
         except KeyboardInterrupt:
-            sys.exit()
+            sys.exit('\nExiting...\n')
